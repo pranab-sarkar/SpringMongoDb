9a82eca3153820c735653773223eec1ef80ec46d
pranab-sarkar <pranab.theprime@gmail.com>
2021-08-09 03:10:25 +0600

    Spring Mongo commit

---
+++ spring-mongodb/src/main/java/service/BookRepository.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, Integer> {
+
+}
